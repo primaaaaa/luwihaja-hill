e69102deb1500c2544aaad524c692689f46865ff
Primaaaaa <primayudhistira040@gmail.com>
2025-11-12 08:41:54 +0700

    Hapus Inertia JS

--- resources/js/Pages/Admin/Reservations/Index.jsx
+++
@@ -1,9 +0,0 @@
-const Index = () => {
-    return (
-        <div>
-            <p>Dashboard Admin</p>
-        </div>
-    );
-};
-
-export default Index;
--- routes/web.php
+++ routes/web.php
@@ -5,26 +5,8 @@ use App\Http\Controllers\AuthController;
 use App\Http\Controllers\PageController;
 use Illuminate\Foundation\Application;
 use Illuminate\Support\Facades\Route;
-use Inertia\Inertia;
 
-Route::get('/', function () {
-    return Inertia::render('Welcome', [
-        'canLogin' => Route::has('login'),
-        'canRegister' => Route::has('register'),
-        'laravelVersion' => Application::VERSION,
-        'phpVersion' => PHP_VERSION,
-    ]);
-});
 
-Route::get('/dashboard', function () {
-    return Inertia::render('Dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
-
-Route::middleware('auth')->group(function () {
-    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
-    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
-    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
-});
 
 Route::get('/login', [AuthController::class, 'showLogin'])->name('login');
 Route::get('/register', [AuthController::class, 'showRegister'])->name('register');
