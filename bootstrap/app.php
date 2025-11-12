686a4f09a9e81a5bdd3b34684447e3b699cb484e
Primaaaaa <primayudhistira040@gmail.com>
2025-11-12 08:59:31 +0700

    fix bootstrap/app.php

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -12,12 +12,12 @@ return Application::configure(basePath: dirname(__DIR__))
     )
     ->withMiddleware(function (Middleware $middleware): void {
         $middleware->web(append: [
-            \App\Http\Middleware\HandleInertiaRequests::class,
+            // \App\Http\Middleware\HandleInertiaRequests::class,
             \Illuminate\Http\Middleware\AddLinkHeadersForPreloadedAssets::class,
         ]);
 
         $middleware->alias([
-            'isAdmin' => \App\Http\Middleware\CheckIsAdmin::class,
+            // 'isAdmin' => \App\Http\Middleware\CheckIsAdmin::class,
         ]);
 
         //
