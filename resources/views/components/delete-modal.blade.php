a999f567d01c0f170a78710f8eeaddb70efe330e
Primaaaaa <primayudhistira040@gmail.com>
2025-11-20 16:08:58 +0700

    Komponen Delete Modal

---
+++ resources/views/components/delete-modal.blade.php
@@ -0,0 +1,28 @@
+@props([
+        'id' => '',
+        'nama' => '',
+        'route' => ''
+    ])    
+
+    
+    <div class="modal fade" id="deleteModal{{ $id }}" tabindex="-1">
+            <div class="modal-dialog modal-dialog-centered delete-modal-dialog">
+                <div class="modal-content delete-modal">
+                    <button type="button" class="btn-close delete-close" data-bs-dismiss="modal"></button>
+                    <div class="modal-body text-center delete-modal-body">
+                        <h6 class="fw-bold mb-3 delete-title">
+                            Apakah Anda yakin ingin<br>menghapus kamar {{ $nama }}?
+                        </h6>
+                        <div class="d-flex gap-2 justify-content-center mt-4">
+                            <button class="btn btn-tidak" data-bs-dismiss="modal">Tidak</button>
+                            <form action="{{ route($route, $id) }}" method="POST"
+                                class="d-inline">
+                                @csrf
+                                @method('DELETE')
+                                <button type="submit" class="btn btn-ya">Ya</button>
+                            </form>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
--- resources/views/pages/admin/manajemenkamar.blade.php
+++ resources/views/pages/admin/manajemenkamar.blade.php
@@ -206,27 +206,7 @@
         </div>
 
         {{-- Delete Modal for each room --}}
-        <div class="modal fade" id="deleteModal{{ $room->id_tipe_villa }}" tabindex="-1">
-            <div class="modal-dialog modal-dialog-centered delete-modal-dialog">
-                <div class="modal-content delete-modal">
-                    <button type="button" class="btn-close delete-close" data-bs-dismiss="modal"></button>
-                    <div class="modal-body text-center delete-modal-body">
-                        <h6 class="fw-bold mb-3 delete-title">
-                            Apakah Anda yakin ingin<br>menghapus kamar {{ $room->nama_unit }}?
-                        </h6>
-                        <div class="d-flex gap-2 justify-content-center mt-4">
-                            <button class="btn btn-tidak" data-bs-dismiss="modal">Tidak</button>
-                            <form action="{{ route('admin.kamar.delete', $room->id_tipe_villa) }}" method="POST"
-                                class="d-inline">
-                                @csrf
-                                @method('DELETE')
-                                <button type="submit" class="btn btn-ya">Ya</button>
-                            </form>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
+        <x-delete-modal :id="$room->id_tipe_villa" :nama="$room->nama_unit" :route="admin.kamar.delete"></x-delete-modal>
         @endforeach
 
     </x-data-table>
