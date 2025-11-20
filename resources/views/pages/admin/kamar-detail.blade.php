82280bd62e3fa2c75e9f069b4a050845f1c8b46f
Arettaaa <aretta557@gmail.com>
2025-11-20 17:10:29 +0700

    push kamar

--- resources/views/pages/admin/kamar-detail.blade.php
+++ resources/views/pages/admin/kamar-detail.blade.php
@@ -9,45 +9,49 @@
         <form>
             <div class="mb-3">
                 <label class="form-label">Nama Unit</label>
-                <input type="text" class="form-control" value="{{ $room->name }}" readonly>
+                <input type="text" class="form-control" value="{{ $kamar->nama_unit }}" readonly>
             </div>
 
             <div class="mb-3">
                 <label class="form-label">Kapasitas Kamar</label>
-                <input type="text" class="form-control" value="{{ $room->capacity }}" readonly>
+                <input type="text" class="form-control" value="{{ $kamar->kapasitas }}" readonly>
             </div>
 
             <div class="row">
                 <div class="col-md-6 mb-3">
                     <label class="form-label">Kode Tipe</label>
-                    <input type="text" class="form-control" value="{{ $room->kode_tipe }}" readonly>
+                    <input type="text" class="form-control" value="{{ $kamar->kode_tipe }}" readonly>
                 </div>
                 <div class="col-md-6 mb-3">
                     <label class="form-label">Status Kamar</label>
-                    <input type="text" class="form-control" value="{{ $room->status }}" readonly>
+                    <input type="text" class="form-control" value="{{ $kamar->status }}" readonly>
                 </div>
             </div>
 
             <div class="row">
                 <div class="col-md-6 mb-3">
                     <label class="form-label">Harga Weekday</label>
-                    <input type="text" class="form-control" value="{{ $room->price_per_night }}" readonly>
+                    <input type="text" class="form-control" value="{{ $kamar->harga_weekday }}" readonly>
                 </div>
                 <div class="col-md-6 mb-3">
                     <label class="form-label">Harga Weekend</label>
-                    <input type="text" class="form-control" value="{{ $room->price_per_night + 100000 }}" readonly>
+                    <input type="text" class="form-control" value="{{ $kamar->harga_weekend }}" readonly>
                 </div>
             </div>
 
             <div class="row">
                 <div class="col-md-6 mb-3">
                     <label class="form-label">Kategori Kamar</label>
-                    <input type="text" class="form-control" value="{{ $room->kategori }}" readonly>
+                    <input type="text" class="form-control" value="{{ $kamar->kategori }}" readonly>
                 </div>
                 <div class="col-md-6 mb-3">
                     <label class="form-label">Foto Kamar</label>
-                    <input type="text" class="form-control" value="{{ $room->main_image }}" readonly>
+                    <input type="text" class="form-control" value="{{ $kamar->foto_kamar }}" readonly>
                 </div>
+            </div>
+ 
+            <div class="mb-4">
+                <label class="form-label">Deskripsi Kamar</label>
                 <textarea class="form-control" rows="3" readonly>{{ $kamar->deskripsi }}</textarea>
             </div>
 
